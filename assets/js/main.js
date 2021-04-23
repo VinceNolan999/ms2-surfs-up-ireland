fe134e14303dc0540d985bb642d4718273ee18fc
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-04-23 11:27:19 +0000

    smooth scolling added

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+// https://webdesign.tutsplus.com/tutorials/smooth-scrolling-vanilla-javascript--cms-35165  
+
+//------------------Smooth Scrolling---------------------//
+
+
+const links = document.querySelectorAll(".page-header ul a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
