e1f8d740f1902be25a049a263f9f8bf1ad83cba1
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-04-23 11:28:01 +0000

    google maps functionality

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,59 +1,59 @@
 //Add Google Maps
 
-function initMap() {
-  const options = {
-    center: { 
-      lat: 36.1783653, 
-      lng: 135.0709152 
-    },
-    zoom: 4
-  };
-  
-  //New map
-  const map = new
-  google.maps.Map(document.getElementById('map'), options);
+//https://fruitbatm.github.io/explore-national-parks-of-japan/   
 
-  //Create markers
-   
-  const markers = [{
+  let map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+           lat: 53.430,
+           lng: -7.94
+         },
+         zoom: 7
+    });
+
+  //Map locations
+
+  const locations = [{
       coordinates: {
-        lat: 35.2326407,
-        lng: 139.013563
+        lat: 52.843446,
+        lng: -9.433542
       },
-      content: '<h5>beach</h5>',
+      content: '<h6>Spanish point beach</h6>',
       info: `<p>aaaa</p>`
     },
     {
-      
+
       coordinates: {
-        lat: 43.5655079, 
-        lng: 144.3598874
-      }, 
-      content: '<h5>beach</h5>',
+        lat: 52.933555,
+        lng: -9.347998
+      },
+      content: '<h5>Lahinch beach</h5>',
       info: `<p>bbbbbbb</p>`
-    } 
+    }
   ];
 
-  //Loop through markers 
-  for (var i = 0; i < markers.length; i++) {
-    addMarker(markers[i]);
+  //Loop through locations 
+  for (var i = 0; i < locations.length; i++) {
+    addMarker(locations[i]);
   }
 
   // Marker function
+
   function addMarker(props) {
     const marker = new google.maps.Marker({
-    position: props.coordinates,
-    map: map,
-  });
-
-    if(props.content) {
-      const infowindow = new google.maps.InfoWindow ({
-        content:props.content
+      position: props.coordinates,
+      map: map,
+    });
+    if (props.content) {
+      const infowindow = new google.maps.InfoWindow({
+        content: props.content
       });
 
       // Open info window when clicked on the marker
-      google.maps.event.addListener(marker, 'click', function() {
-        if(!marker.open) {
+      google.maps.event.addListener(marker, 'click', function () {
+        if (!marker.open) {
           infowindow.open(map, marker);
           marker.open = true;
         }
@@ -63,19 +63,32 @@ function initMap() {
           marker.open = false;
         }
         // Close info window when clicked anywhere on the map and the info window was opened
-        google.maps.event.addListener(map, 'click', function() {
+        google.maps.event.addListener(map, 'click', function () {
           infowindow.close();
           marker.open = false;
+        });
       });
-    });
 
+        //  Zoom into marker
+      google.maps.event.addListener(marker,'click',function() {
+        map.setZoom(10);
+        map.setCenter(marker.getPosition());
+        });
+
+        
       // Info shows in the side column when the marker is clicked
-      google.maps.event.addListener(marker, 'click',  (function(i) {
+      google.maps.event.addListener(marker, 'click', (function (i) {
 
-        return function() {
-          document.getElementById('map-content').innerHTML = markers[i].info;
+        return function () {
+          document.getElementById('map-content').innerHTML = locations[i].info;
         };
       })(i));
     }
   }
 }
+
+//test image zoom
+$(document).ready(function(){
+  $('#kb > img').addClass('zoom');
+});
+
