572a22814ee274dfa177f45460509f7e018b6879
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-04-26 10:52:42 +0000

    final list of locations added.

---
+++ assets/js/locations.js
@@ -0,0 +1,104 @@
+const locations = [{
+    coordinates: {
+      lat: 54.483,
+      lng: -8.277
+    },
+    content: '<h6>bundoran beach</h6>',
+    info: `<p>bundoran</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 54.212,
+      lng: -10.073
+    },
+    content: '<h6>inishcrone beach</h6>',
+    info: `<p>inishcrone</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 53.971,
+      lng: -10.073
+    },
+    content: '<h6>keel beach</h6>',
+    info: `<p>keel</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 52.1575,
+      lng: -7.1354
+    },
+    content: '<h6>tranmore beach</h6>',
+    info: `<p>tranmore</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 52.933555,
+      lng: -9.347998
+    },
+    content: '<h5>lahinch beach</h5>',
+    info: `<p>bbbbbbb</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 52.843446,
+      lng: -9.433542
+    },
+    content: '<h6>spanish point beach</h6>',
+    info: `<p>aaaa</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 52.764,
+      lng: -9.492
+    },
+    content: '<h6>doonbeg beach</h6>',
+    info: `<p>doonbeg</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 52.2609,
+      lng: -10.0138
+    },
+    content: '<h6>castle gregory beach</h6>',
+    info: `<p>castle gregory</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 52.1392,
+      lng: -9.9819
+    },
+    content: '<h6>inch strand beach</h6>',
+    info: `<p>inch strand</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 51.5961,
+      lng: -8.8638
+    },
+    content: '<h6>inchydoney beach</h6>',
+    info: `<p>incheydoney</p>`
+  },
+
+  {
+    coordinates: {
+      lat: 51.644,
+      lng: -8.5809
+    },
+    content: '<h6>garretstown beach</h6>',
+    info: `<p>garretstown</p>`
+  },
+
+  
+];
+
+
+
